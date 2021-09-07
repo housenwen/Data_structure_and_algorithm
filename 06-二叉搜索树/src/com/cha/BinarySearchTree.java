6569cf4b39b15cd2469d8c902f8b2c205d9bc88c
housenwen <1300817980@qq.com.com>
2021-09-07 20:29:20 +0800

    06-二叉搜索树

---
+++ 06-二叉搜索树/src/com/cha/BinarySearchTree.java
@@ -0,0 +1,125 @@
+package com.cha;
+
+import java.util.Comparator;
+
+import com.cha.printer.BinaryTreeInfo;
+
+@SuppressWarnings("unchecked")
+public class BinarySearchTree<E> implements BinaryTreeInfo {
+	private int size;
+	private Node<E> root;
+	private Comparator<E> comparator;
+	
+	public BinarySearchTree() {
+		this(null);
+	}
+	
+	public BinarySearchTree(Comparator<E> comparator) {
+		this.comparator = comparator;
+	}
+	
+	public int size() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public void clear() {
+		
+	}
+
+	public void add(E element) {
+		elementNotNullCheck(element);
+		
+		// 添加第一个节点
+		if (root == null) {
+			root = new Node<>(element, null);
+			size++;
+			return;
+		}
+		
+		// 添加的不是第一个节点
+		// 找到父节点
+		Node<E> parent = root;
+		Node<E> node = root;
+		int cmp = 0;
+		while (node != null) {
+			cmp = compare(element, node.element);
+			parent = node;
+			if (cmp > 0) {
+				node = node.right;
+			} else if (cmp < 0) {
+				node = node.left;
+			} else { // 相等
+				return;
+			}
+		}
+
+		// 看看插入到父节点的哪个位置
+		Node<E> newNode = new Node<>(element, parent);
+		if (cmp > 0) {
+			parent.right = newNode;
+		} else {
+			parent.left = newNode;
+		}
+		size++;
+	}
+
+	public void remove(E element) {
+		
+	}
+
+	public boolean contains(E element) {
+		return false;
+	}
+	
+	/**
+	 * @return 返回值等于0，代表e1和e2相等；返回值大于0，代表e1大于e2；返回值小于于0，代表e1小于e2
+	 */
+	private int compare(E e1, E e2) {
+		if (comparator != null) {
+			return comparator.compare(e1, e2);
+		}
+		return ((Comparable<E>)e1).compareTo(e2);
+	}
+	
+	private void elementNotNullCheck(E element) {
+		if (element == null) {
+			throw new IllegalArgumentException("element must not be null");
+		}
+	}
+
+	private static class Node<E> {
+		E element;
+		Node<E> left;
+		Node<E> right;
+		@SuppressWarnings("unused")
+		Node<E> parent;
+		public Node(E element, Node<E> parent) {
+			this.element = element;
+			this.parent = parent;
+		}
+	}
+
+	@Override
+	public Object root() {
+		return root;
+	}
+
+	@Override
+	public Object left(Object node) {
+		return ((Node<E>)node).left;
+	}
+
+	@Override
+	public Object right(Object node) {
+		return ((Node<E>)node).right;
+	}
+
+	@Override
+	public Object string(Object node) {
+		return ((Node<E>)node).element;
+	}
+}
---
+++ 06-二叉搜索树/src/com/cha/Main.java
@@ -0,0 +1,125 @@
+package com.cha;
+
+import java.util.Comparator;
+
+import com.cha.printer.BinaryTreeInfo;
+import com.cha.printer.BinaryTrees;
+
+public class Main {
+	
+	private static class PersonComparator implements Comparator<Person> {
+		public int compare(Person e1, Person e2) {
+			return e1.getAge() - e2.getAge();
+		}
+	}
+	
+	private static class PersonComparator2 implements Comparator<Person> {
+		public int compare(Person e1, Person e2) {
+			return e2.getAge() - e1.getAge();
+		}
+	}
+
+	static void test1() {
+		Integer data[] = new Integer[] {
+				7, 4, 9, 2, 5, 8, 11, 3, 12, 1
+		};
+		
+		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+		for (int i = 0; i < data.length; i++) {
+			bst.add(data[i]);
+		}
+		
+		BinaryTrees.println(bst);
+	}
+	
+	static void test2() {
+
+		Integer data[] = new Integer[] {
+				7, 4, 9, 2, 5, 8, 11, 3, 12, 1
+		};
+		
+		BinarySearchTree<Person> bst1 = new BinarySearchTree<>();
+		for (int i = 0; i < data.length; i++) {
+			bst1.add(new Person(data[i]));
+		}
+		
+		BinaryTrees.println(bst1);
+		
+		BinarySearchTree<Person> bst2 = new BinarySearchTree<>(new Comparator<Person>() {
+			public int compare(Person o1, Person o2) {
+				return o2.getAge() - o1.getAge();
+			}
+		});
+		for (int i = 0; i < data.length; i++) {
+			bst2.add(new Person(data[i]));
+		}
+		BinaryTrees.println(bst2);
+	}
+	
+	static void test3() {
+		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+		for (int i = 0; i < 30; i++) {
+			bst.add((int)(Math.random() * 100));
+		}
+		BinaryTrees.println(bst);
+	}
+	
+	public static void main(String[] args) {
+		BinaryTrees.println(new BinaryTreeInfo() {
+			
+			@Override
+			public Object string(Object node) {
+				return node.toString() + "_";
+			}
+			
+			@Override
+			public Object root() {
+				return "A";
+			}
+			
+			@Override
+			public Object right(Object node) {
+				if (node.equals("A")) return "C";
+				if (node.equals("C")) return "E";
+				return null;
+			}
+			
+			@Override
+			public Object left(Object node) {
+				if (node.equals("A")) return "B";
+				if (node.equals("C")) return "D";
+				return null;
+			}
+		});
+		
+		// test3();
+		
+		
+		/*
+		 * Java的匿名类，类似于iOS中的Block、JS中的闭包（function）
+		 */
+		
+//		BinarySearchTree<Person> bst1 = new BinarySearchTree<>(new Comparator<Person>() {
+//			@Override
+//			public int compare(Person o1, Person o2) {
+//				return 0;
+//			}
+//		});
+//		bst1.add(new Person(12));
+//		bst1.add(new Person(15));
+//		
+//		BinarySearchTree<Person> bst2 = new BinarySearchTree<>(new PersonComparator());
+//		bst2.add(new Person(12));
+//		bst2.add(new Person(15));
+//
+		
+		
+//		BinarySearchTree<Car> bst4 = new BinarySearchTree<>(new Car);
+//		
+//		
+//		java.util.Comparator<Integer> iComparator;
+		
+		
+	}
+
+}
