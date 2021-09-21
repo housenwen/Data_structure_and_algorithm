924f5a2e4141fe1a34deaf748ca5ba4df8228da4
housenwen <1300817980@qq.com.com>
2021-09-21 17:17:23 +0800

    动态规划

---
+++ 16-排序/src/com/cha/sort/cmp/BubbleSort3.java
@@ -0,0 +1,22 @@
+package com.cha.sort.cmp;
+
+import com.cha.sort.Sort;
+
+public class BubbleSort3<T extends Comparable<T>> extends Sort<T> {
+
+	@Override
+	protected void sort() {
+		for (int end = array.length - 1; end > 0; end--) {
+			int sortedIndex = 1;
+			for (int begin = 1; begin <= end; begin++) {
+				// if (array[begin] < array[begin - 1]) {
+				if (cmp(begin, begin - 1) < 0) {
+					swap(begin, begin - 1);
+					sortedIndex = begin;
+				}
+			}
+			end = sortedIndex;
+		}
+	}
+
+}
